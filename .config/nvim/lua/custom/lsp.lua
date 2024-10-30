417f785e400558d4aac2dc25a695db2e1771435a
Jayesh Kale <jayesh6297@gmail.com>
2024-10-30 12:29:01 +0530

    lua enabled hints

--- .config/nvim/lua/custom/lsp.lua
+++ .config/nvim/lua/custom/lsp.lua
@@ -70,9 +70,9 @@ M.lua = function()
 					enable = true,
 					setType = false,
 					paramType = true,
-					paramName = "Disable",
-					semicolon = "Disable",
-					arrayIndex = "Disable",
+					paramName = true,
+					semicolon = false,
+					arrayIndex = true,
 				},
 				codeLens = {
 					enable = true,

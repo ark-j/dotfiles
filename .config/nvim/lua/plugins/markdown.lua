751e801d6f57a47a353b9265de30dce75d79d7ee
Jayesh Kale <jayesh6297@gmail.com>
2024-11-13 20:31:57 +0530

    added markdown commit

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,7 +1,15 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim", "nvim-tree/nvim-web-devicons" },
-	---@module 'render-markdown'
-	---@type render.md.UserConfig
-	opts = {},
+	config = function()
+		require("render-markdown").setup({
+			file_types = { "markdown" },
+			render_modes = { "n", "c" },
+			heading = {
+				enabled = true,
+				sign = false,
+				backgrounds = {},
+			},
+		})
+	end,
 }

c8bdbf0a2a3473b3fa823db0979a822c43252fbf
Jayesh Kale <jayesh6297@gmail.com>
2025-01-24 23:57:48 +0530

    (replace) nvim-autopairs with mini.pairs

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -12,8 +12,13 @@ return { -- Collection of various small independent plugins/modules
     -- Add/delete/replace surroundings (brackets, quotes, etc.)
     --
     -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+    -- - sa$)  - [S]urrounf [A]dd [$]end [)]
+    -- - sawb  - [S]urround [A]dd [W]ord [B]racket
     -- - sd'   - [S]urround [D]elete [']quotes
-    -- - sr)'  - [S]urround [R]eplace [)] [']
+    -- - sr)'  - [S]urround [R]eplace [)] with [']
+    -- - sr"'  - [S]urround [R]replace ["] with [']
     require("mini.surround").setup()
+
+    require("mini.pairs").setup()
   end,
 }

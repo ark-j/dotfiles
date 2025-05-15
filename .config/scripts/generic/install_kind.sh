b920a23d4360944848599c0a44f14202f4730c76
Jayesh Kale <jayesh6297@gmail.com>
2025-05-15 18:39:46 +0530

    fix: script debug

--- .config/scripts/generic/install_kind.sh
+++ .config/scripts/generic/install_kind.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -xo pipefail
+set -o pipefail
 KIND_VERSION=$(curl -sX GET https://api.github.com/repos/kubernetes-sigs/kind/releases/latest | grep '"tag_name":' | awk '{gsub(/[",]/, "", $2); print $2}')
 SYSTEM_NAME=$(uname | awk '{print tolower($0)}')
 SYSTEM_ARCH=$(uname -m)
@@ -10,4 +10,5 @@ if [ -n "$KIND_VERSION" ]; then
     curl -Lo /tmp/kind "https://github.com/kubernetes-sigs/kind/releases/download/${KIND_VERSION}/kind-${SYSTEM_NAME}-${SYSTEM_ARCH}"
     chmod +x /tmp/kind
     sudo mv /tmp/kind /usr/local/bin
+    echo "installed kind version ${KIND_VERSION}-${SYSTEM_NAME}-${SYSTEM_ARCH}"
 fi

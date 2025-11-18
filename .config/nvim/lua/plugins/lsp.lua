ee395a92291bb550c0c28cf0d99306756e7b2ca9
Jayesh Kale <jayesh6297@gmail.com>
2025-11-18 12:53:09 +0530

    nvim: introducing blink cmp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,57 +1,56 @@
 -- LSP Configuration & Pluginslsp
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    { "williamboman/mason.nvim", config = true },
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    -- required for yaml companion
-    "someone-stole-my-name/yaml-companion.nvim",
-    "b0o/SchemaStore.nvim",
-  },
-  config = function()
-    require("mason").setup({
-      PATH = "skip",
-      ui = {
-        border = "rounded",
-        icons = {
-          package_pending = " ",
-          package_installed = " ",
-          package_uninstalled = " ",
-        },
-      },
-      max_concurrent_installers = 5,
-    })
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		{ "williamboman/mason.nvim", config = true },
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		-- required for yaml companion
+		"someone-stole-my-name/yaml-companion.nvim",
+		"b0o/SchemaStore.nvim",
+	},
+	config = function()
+		require("mason").setup({
+			PATH = "skip",
+			ui = {
+				icons = {
+					package_pending = " ",
+					package_installed = " ",
+					package_uninstalled = " ",
+				},
+			},
+			max_concurrent_installers = 5,
+		})
 
-    require("custom.lsp")
+		require("custom.lsp")
 
-    vim.diagnostic.config({
-      severity_sort = true,
-      float = { border = "rounded", source = "if_many" },
-      virtual_lines = true,
-      virtual_text = false,
-      signs = {
-        text = {
-          [vim.diagnostic.severity.ERROR] = " ",
-          [vim.diagnostic.severity.WARN] = "󰀪 ",
-          [vim.diagnostic.severity.INFO] = " ",
-          [vim.diagnostic.severity.HINT] = " ",
-        },
-        numhl = {
-          [vim.diagnostic.severity.ERROR] = "",
-          [vim.diagnostic.severity.WARN] = "",
-          [vim.diagnostic.severity.HINT] = "",
-          [vim.diagnostic.severity.INFO] = "",
-        },
-      },
-    })
+		vim.diagnostic.config({
+			severity_sort = true,
+			float = { source = "if_many" },
+			virtual_lines = true,
+			virtual_text = false,
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = " ",
+					[vim.diagnostic.severity.WARN] = "󰀪 ",
+					[vim.diagnostic.severity.INFO] = " ",
+					[vim.diagnostic.severity.HINT] = " ",
+				},
+				numhl = {
+					[vim.diagnostic.severity.ERROR] = "",
+					[vim.diagnostic.severity.WARN] = "",
+					[vim.diagnostic.severity.HINT] = "",
+					[vim.diagnostic.severity.INFO] = "",
+				},
+			},
+		})
 
-    vim.keymap.set("n", "<leader>l", function()
-      local config = vim.diagnostic.config() or {}
-      if config.virtual_text then
-        vim.diagnostic.config({ virtual_text = false, virtual_lines = true })
-      else
-        vim.diagnostic.config({ virtual_text = true, virtual_lines = false })
-      end
-    end, { desc = "Toggle lsp_lines" })
-  end,
+		vim.keymap.set("n", "<leader>l", function()
+			local config = vim.diagnostic.config() or {}
+			if config.virtual_text then
+				vim.diagnostic.config({ virtual_text = false, virtual_lines = true })
+			else
+				vim.diagnostic.config({ virtual_text = true, virtual_lines = false })
+			end
+		end, { desc = "Toggle lsp_lines" })
+	end,
 }
--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,24 +1,23 @@
 return { -- Collection of various small independent plugins/modules
-  "echasnovski/mini.nvim",
-  config = function()
-    -- Better Around/Inside textobjects
-    --
-    -- Examples:
-    --  - va)  - [V]isually select [A]round [)]paren
-    --  - yinq - [Y]ank [I]nside [N]ext [']quote
-    --  - ci'  - [C]hange [I]nside [']quote
-    require("mini.ai").setup({ n_lines = 500 })
+	"echasnovski/mini.nvim",
+	config = function()
+		-- Better Around/Inside textobjects
+		--
+		-- Examples:
+		--  - va)  - [V]isually select [A]round [)]paren
+		--  - yinq - [Y]ank [I]nside [N]ext [']quote
+		--  - ci'  - [C]hange [I]nside [']quote
+		require("mini.ai").setup({ n_lines = 500 })
 
-    -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    --
-    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-    -- - sa$)  - [S]urrounf [A]dd [$]end [)]
-    -- - sawb  - [S]urround [A]dd [W]ord [B]racket
-    -- - sd'   - [S]urround [D]elete [']quotes
-    -- - sr)'  - [S]urround [R]eplace [)] with [']
-    -- - sr"'  - [S]urround [R]replace ["] with [']
-    require("mini.surround").setup()
-
-    require("mini.pairs").setup()
-  end,
+		-- Add/delete/replace surroundings (brackets, quotes, etc.)
+		--
+		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+		-- - sa$)  - [S]urrounf [A]dd [$]end [)]
+		-- - sawb  - [S]urround [A]dd [W]ord [B]racket
+		-- - sd'   - [S]urround [D]elete [']quotes
+		-- - sr)'  - [S]urround [R]eplace [)] with [']
+		-- - sr"'  - [S]urround [R]replace ["] with [']
+		require("mini.surround").setup()
+		require("mini.pairs").setup()
+	end,
 }
